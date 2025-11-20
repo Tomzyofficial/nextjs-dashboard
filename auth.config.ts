425fc1766c6933c566c95c7ac0a30af4db4c14b3
Tomzyofficial <tomzyofficial54@gmail.com>
2025-11-20 22:10:46 +0100

    Middleware issue fixed

--- auth.config.ts
+++ auth.config.ts
@@ -8,11 +8,14 @@ export const authConfig = {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
+      const isOnLogin = nextUrl.pathname.startsWith("/login");
+
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL("/dashboard", nextUrl));
+      } else if (isLoggedIn && isOnLogin) {
+        // Redirect logged-in users away from login page to dashboard
+        return "/dashboard";
       }
       return true;
     },
